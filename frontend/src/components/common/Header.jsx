1771677ebb3916bd9ecb45d5e3600915fd5cdb1e
Sulagna Pradhan <152047288+Sulagna-pradhan@users.noreply.github.com>
2025-04-25 22:13:15 +0530

    Update Header.jsx

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -42,7 +42,7 @@ export default function Header() {
           <div className="flex items-center">
           <Link to="/" className="flex items-center">
             <img src="/quiz.png" alt="Company Logo" className="h-8 mr-3" />
-            <span className="text-2xl font-bold text-blue-600">TestNity</span>
+            <span className="text-2xl font-bold text-blue-600">Testnity</span>
             </Link>
           </div>
 
